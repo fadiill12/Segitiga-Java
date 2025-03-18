01098d4d9230ab74d2e829b79609be53d2c0162b
FADILAH AKBAR <cocgaming225@gmail.com>
2025-03-18 19:43:26 +0700

    Update Main.java

--- src/com/tugas/Main.java
+++ src/com/tugas/Main.java
@@ -1,4 +1,20 @@
 package com.tugas;
+import java.util.*;
 
 public class Main {
+
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---------- LUAS SEGITIGA ----------");
+        System.out.print("MASUKAN ALAS SEGITIGA\t\t= ");
+        double alas = scanner.nextDouble();
+        System.out.print("MASUKAN TINGGI SEGITIGA\t\t= ");
+        double tinggi = scanner.nextDouble();
+
+        Segitiga segitiga = new Segitiga(alas, tinggi);
+        System.out.println("LUAS SEGITIGA ADALAH\t\t= " + segitiga.hitungLuas());
+        System.out.println("---------- LUAS SEGITIGA ----------");
+
+    }
 }
